20d2429369acfc4675b5ea89a656941eed8ae46e
Manan Goel <manan.goel@research.iiit.ac.in>
2019-08-21 23:45:04 +0530

    Implemented cd for absolute paths

--- shell_cd.c
+++ shell_cd.c
@@ -4,56 +4,67 @@ int shell_cd(char** args, char* root) {
 
     char* token = strtok(args[1], "/");
 
-    while (token != NULL) { 
-        char* cwd = (char*)malloc(8192);
-        getcwd(cwd, 8192);
-
-        if (strcmp(token, "~") == 0) {
-            chdir(root);
+    if (args[1][0] == '/') {
+        int error = chdir(args[1]);
+        if (error < 0) {
+            perror("Error");
         }
+        // printf("%s\n", token);
+    }
+    else {
+        while (token != NULL) { 
+            char* cwd = (char*)malloc(8192);
+            getcwd(cwd, 8192);
 
-        else if (strcmp(token, "..") == 0) {
-            char* prev_dir = (char*)malloc(8192);
+            if (strcmp(token, "~") == 0) {
+                chdir(root);
+            }
 
-            int length = strlen(cwd);
-            int i = length;
-            for (i = length - 1; i >=0; i--) {
-                if (cwd[i] == '/') {
-                    break;
+            else if (strcmp(token, "..") == 0) {
+                char* prev_dir = (char*)malloc(8192);
+
+                int length = strlen(cwd);
+                int i = length;
+                for (i = length - 1; i >=0; i--) {
+                    if (cwd[i] == '/') {
+                        break;
+                    }
                 }
+
+                for (int j = 0; j < i; j++) {
+                    prev_dir[j] = cwd[j];
+                }
+                prev_dir[i] = '\0';
+
+                int error = chdir(prev_dir);
+                if (error < 0) {
+                    perror("Error");
+                }
+                free(prev_dir);
             }
 
-            for (int j = 0; j < i; j++) {
-                prev_dir[j] = cwd[j];
-            }
-            prev_dir[i] = '\0';
-
-            int error = chdir(prev_dir);
-            if (error < 0) {
-                perror("Error");
-            }
-            free(prev_dir);
-        }
-
-        else if (strcmp(token, ".") == 0) {
-            chdir(cwd);
-        }
-
-        else {
-            char* dir = (char*)malloc(8192);
-            strcat(dir, cwd);
-            strcat(dir, "/");
-            strcat(dir, token);
-            int error = chdir(dir);
-
-            if (error < 0) {
-                perror("Error");
+            else if (strcmp(token, ".") == 0) {
+                chdir(cwd);
             }
 
-        }
+            else {
+                char* dir = (char*)malloc(8192);
+                strcat(dir, cwd);
+                strcat(dir, "/");
+                strcat(dir, token);
+                int error = chdir(dir);
 
-        free(cwd);
-        token = strtok(NULL, "/"); 
+                if (error < 0) {
+                    perror("Error");
+                }
+
+            }
+
+            free(cwd);
+            token = strtok(NULL, "/"); 
+        } 
+
     }
 
+    
 }
